c8da26c2d7a68428aa772d12397443d7055e5d0e
Shin <86329731+Shin-Ideal@users.noreply.github.com>
2024-06-30 02:36:38 +0900

    水平方向の移動制限を突破しようとするプレイヤーを内側にテレポートする機能を追加

--- src/main/java/com/github/Shin_Ideal/LobbyFlyPlugin/Listeners/Horizon_Limit_Listener.java
+++ src/main/java/com/github/Shin_Ideal/LobbyFlyPlugin/Listeners/Horizon_Limit_Listener.java
@@ -1,5 +1,6 @@
 package com.github.Shin_Ideal.LobbyFlyPlugin.Listeners;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -29,18 +30,38 @@ public class Horizon_Limit_Listener implements Listener {
         double z = player.getLocation().getZ();
 
         if (x < x_min_distance) {
+            if (x < x_min_distance - 100) {
+                Location location = player.getLocation();
+                location.setX(x_min_distance);
+                player.teleport(location);
+            }
             player.setVelocity(player_vector.add(new Vector(1, 0, 0)));
         }
 
         if (x_max_distance < x) {
+            if (x_max_distance + 100 < x) {
+                Location location = player.getLocation();
+                location.setX(x_max_distance);
+                player.teleport(location);
+            }
             player.setVelocity(player_vector.add(new Vector(-1, 0, 0)));
         }
 
         if (z < z_min_distance) {
+            if (z < z_min_distance - 100) {
+                Location location = player.getLocation();
+                location.setZ(z_min_distance);
+                player.teleport(location);
+            }
             player.setVelocity(player_vector.add(new Vector(0, 0, 1)));
         }
 
         if (z_max_distance < z) {
+            if (z_max_distance + 100 < z) {
+                Location location = player.getLocation();
+                location.setZ(z_max_distance);
+                player.teleport(location);
+            }
             player.setVelocity(player_vector.add(new Vector(0, 0, -1)));
         }
     }
